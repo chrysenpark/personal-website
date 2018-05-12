fe353253165468b49f6b1da6dbfca718e2c10a52
ChrysenPark <chrysen777@gmail.com>
2018-05-11 20:35:27 -0700

    first attempt

---
+++ js/intro_typing.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  setTimeout(function(){
+    $("#typed-name").typed({
+      strings: ["Chrysen Park"],
+      contentType: 'text',
+      typeSpeed: 100,
+      loop: false,
+      showCursor: false,
+    });
+
+  }, 0);
+});
